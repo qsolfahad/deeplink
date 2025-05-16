0bf331e56b630538ec10803a8038c26817f4ea62
Fahad <qsolfahad@gmail.com>
2025-05-16 19:10:02 +0500

    remove vercel jaon

--- server.js
+++ server.js
@@ -11,12 +11,27 @@ app.post('/create-session', async (req, res) => {
   try {
     const credentials = 'TESTATMOSPHERGYM:607b0aa9d9f0e558118b9a9827bbc47e';
     const authHeader = 'Basic ' + Buffer.from(credentials).toString('base64');
-
+    const payload = {
+      apiOperation: "INITIATE_CHECKOUT",
+      checkoutMode: "WEBSITE",
+      interaction: {
+        operation: "PURCHASE",
+        merchant: {
+          name: "JK Enterprises LLC",
+          url: "https://www.your.site.url.com"
+        },
+        returnUrl: "https://www.your.site.url.com" // must be a real URL
+      },
+      order: {
+        currency: "PKR",
+        amount: "250.00",
+        id: "ORDER-" + Math.floor(Math.random() * 100000), // generate unique ID
+        description: "Goods and Services"
+      }
+    };
     const response = await axios.post(
       'https://test-bankalfalah.gateway.mastercard.com/api/rest/version/100/merchant/TESTATMOSPHERGYM/session',
-      {
-     
-      },// Empty body for session creation
+      payload,
       {
         headers: {
           'Content-Type': 'application/json',
