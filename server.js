32e92d716b9099d2a68fe937d891bdba6c92acc8
Fahad <qsolfahad@gmail.com>
2025-05-16 18:19:03 +0500

    ADDED INDEX.HTML AND SERVER UPDATE

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); // Allow all origins — for dev only
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+app.post('/create-session', async (req, res) => {
+  try {
+    const credentials = 'TESTATMOSPHERGYM:607b0aa9d9f0e558118b9a9827bbc47e';
+    const authHeader = 'Basic ' + Buffer.from(credentials).toString('base64');
+
+    const response = await axios.post(
+      'https://test-bankalfalah.gateway.mastercard.com/api/rest/version/100/merchant/TESTATMOSPHERGYM/session',
+      {}, // Empty body for session creation
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Authorization': authHeader,
+          Authorization: `Basic bWVyY2hhbnQuVEVTVEFUTU9TUEhFUkdZTTo2MDdiMGFhOWQ5ZjBlNTU4MTE4YjlhOTgyN2JiYzQ3ZQ==`,
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (err) {
+    console.error('Error creating session:', err.response?.data || err.message);
+    res.status(500).json({ error: err.response?.data || err.message });
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
